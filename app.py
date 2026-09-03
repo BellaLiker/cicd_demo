8be5f505c4b99a3748d876e887b02afa2f707832
BellaLiker <phanna069@gmail.com>
2026-09-04 01:04:37 +0700

    test automatic ci cd deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Deployed via CI/CD Pipeline with Docker!"
+    return return "Day 16 CI/CD Deployment SUCCESS!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
